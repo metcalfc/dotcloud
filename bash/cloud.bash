f2a0e1f153acb61610a21d68d6247f89f1836ca8
Chad Metcalf <metcalfc@gmail.com>
2023-10-03 16:54:26 +0000

    Adding remote checks.

---
+++ bash/cloud.bash
@@ -0,0 +1,15 @@
+function is_gitpod() {
+    test -e /usr/bin/gp && test -v GITPOD_REPO_ROOT;
+}
+
+function is_codespaces() {
+    test -v CODESPACES || test -e /home/codespace;
+}
+
+function is_daytona() {
+    test -e /.ca/ca && test -v DAYTONA_WS_ID;   
+}
+
+function is_remote {
+    is_daytona || is_codespaces || is_gitpod;
+}
